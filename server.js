b96a5d7f80c7e814886b65a3834c6506b7467da8
Mona Lisa <joannagrosshans2@gmail.com>
2020-12-14 21:12:57 -0600

    server created and started

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const routes = require('./controllers');
+const mysql= require('mysql');
+const sequelize = require('/.config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const session = require('express-session');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Secret to be guarded to env file',
+    cookie: {},
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.statis(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
