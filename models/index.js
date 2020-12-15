e051d3766adfa6b2b4d20588a6d141e2963be7fc
jess-smith49 <js.jessicasmith774@gmail.com>
2020-12-14 21:21:53 -0600

    import models for associations

--- models/index.js
+++ models/index.js
@@ -3,10 +3,26 @@ const User = require('./User');
 const Drink = require('./Drink');
 const Recipe = require('./Recipe');
 const List = require('./List');
+const Group = require('./Group');
+const Category = require('./Category');
 
 
 //Creating Associations
 
+//Users belong to many groups
+
+//Categories have many Lists
+
+
+
+
+
+
+
+
+
+
+
 
 //Exporting the Models
-module.exports = {User, Drink, Recipe, List};
+module.exports = {User, Drink, Recipe, List, Category, Group};
