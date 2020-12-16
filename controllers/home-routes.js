e197aaf7482b5ff142e2993be1a296cf66fce0c5
Mona Lisa <joannagrosshans2@gmail.com>
2020-12-15 19:44:22 -0600

    home-routes updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,20 +1,91 @@
 const router = require('express').Router();
-const { post } = require('.');
-const {} = require('../models');
+const { Recipe, List, Drink } = require('../models');
 const withAuth = require('../utils/auth');
 
-//get all posts
+
 //get posts from recipes
 router.get('/', withAuth, (req, res) => {
-    Post.findAll({
+    Recipe.findAll({
         attributes: [
-
+            'id',
+            'recipe_name',
+            'ingredients',
+            'instructions'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
         ]
     })
-})
+    .then(dbRecipeData => {
+        const recipes = dbRecipeData.map(recipe => recipe.get({ plain: true }));
+        res.render('homepage', {
+            recipes,
+            loggedIn: req.session.loggedIn 
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 //get posts from drinks
+router.get('/', withAuth, (req, res) => {
+    Drink.findAll({
+        attributes: [
+            'id',
+            'drink_name',
+            'ingredients',
+            'instructions'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+        ]
+    })
+    .then(dbDrinkData => {
+        const drinks = dbDrinkData.map(drink => drink.get({ plain: true }));
+        res.render('homepage', {
+            drinks,
+            loggedIn: req.session.loggedIn 
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 //get posts from gift list
-
+router.get('/', withAuth, (req, res) => {
+    List.findAll({
+        attributes: [
+            'id',
+            'list_name',
+            'list_items'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+        ]
+    })
+    .then(dbListData => {
+        const lists = dbListData.map(list => list.get({ plain: true }));
+        res.render('homepage', {
+            lists,
+            loggedIn: req.session.loggedIn 
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
         res.redirect('/');
