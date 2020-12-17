37668278ac85ed895b7b602c881963e4ad72659d
Mona Lisa <joannagrosshans2@gmail.com>
2020-12-16 20:44:16 -0600

    routes working except for included data

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const drinkRoutes = require('./drink-routes');
 const recipeRoutes = require('./recipe-routes');
 const listRoutes = require('./list-routes');
+const userRoutes = require('./user-routes');
 
 router.use('/lists', listRoutes);
 router.use('/recipes', recipeRoutes);
 router.use('/drinks', drinkRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
 
