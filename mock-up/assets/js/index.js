25789eb7d472aab7dab7150af0dc5a482210d1d5
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-12-17 22:47:22 -0600

    countdown completed

--- mock-up/assets/js/index.js
+++ mock-up/assets/js/index.js
@@ -1,59 +1,60 @@
 window.onload = function() {
     christmasCountdown();
-
-    function christmasCountdown() {
-    //get today's date
-    let now = new Date();
-
-    //get current month
-    let currentMonth = (now.getMonth() + 1);
-
-    //get current day
-    let currentDay = now.getDate();
-
-    //calculate the year that the next Christmas will appear in
-    let nextChristmas = now.getFullYear();
-    if(currentMonth === 12 && currentDay > 25 ) {
-        nextChristmas = nextChristmas + 1;
-    }
-
-    let nextChristmasDate = nextChristmas + '-12-25T00:00:00.000Z';
-    let christmasDay = new Date(nextChristmasDate);
-
-        //Get the difference in seconds between the two days.
-        var diffSeconds = Math.floor((christmasDay.getTime() - now.getTime()) / 1000);
+  function christmasCountdown() {
+    var now = new Date();
  
-        let days = 0;
-        let hours = 0;
-        let minutes = 0;
-    
-     
-        //Don't calculate the time left if it is Christmas day.
-        if(currentMonth != 12 || (currentMonth == 12 && currentDay != 25)){
-            //Convert these seconds into days, hours, minutes, seconds.
-            let days = Math.floor(diffSeconds / (3600*24));
-            diffSeconds  -= days * 3600 * 24;
-            let hours   = Math.floor(diffSeconds / 3600);
-            diffSeconds  -= hours * 3600;
-            let minutes = Math.floor(diffSeconds / 60);
-            diffSeconds  -= minutes * 60;
-          
-        }
+    //Get the current month. Add a +1 because
+    //getMonth starts at 0 for January.
+    var currentMonth = (now.getMonth() + 1);
+ 
+    //Get the current day of the month.
+    var currentDay = now.getDate();
+ 
+    //Work out the year that the next Christmas
+    //day will occur on.
+    var nextChristmasYear = now.getFullYear();
+    if(currentMonth == 12 && currentDay > 25){
+        //This year's Christmas Day has already passed.
+        nextChristmasYear = nextChristmasYear + 1;
+    }
+ 
+    var nextChristmasDate = nextChristmasYear + '-12-25T00:00:00.000Z';
+    var christmasDay = new Date(nextChristmasDate);
+ 
+    //Get the difference in seconds between the two days.
+    var diffSeconds = Math.floor((christmasDay.getTime() - now.getTime()) / 1000);
+ 
+    var days = 0;
+    var hours = 0;
+    var minutes = 0;
+    var seconds = 0;
+ 
+    //Don't calculate the time left if it is Christmas day.
+    if(currentMonth != 12 || (currentMonth == 12 && currentDay != 25)){
+        //Convert these seconds into days, hours, minutes, seconds.
+        days = Math.floor(diffSeconds / (3600*24));
+        diffSeconds  -= days * 3600 * 24;
+        hours   = Math.floor(diffSeconds / 3600);
+        diffSeconds  -= hours * 3600;
+        minutes = Math.floor(diffSeconds / 60);
+        diffSeconds  -= minutes * 60;
+        seconds = diffSeconds;
+    }
+ 
         console.log(days, hours, minutes);
 
-        let daysUntilChristmas = days + ':' + hours + ':' + minutes;
+        let daysUntilChristmas = days;
         console.log(daysUntilChristmas)
         let countdownContainer = document.getElementById('countdown');
-        let  titleEl = document.createElement("h1");
-        titleEl.classList.add('cta-title');
-        titleEl.textContent = `${daysUntilChristmas} Day's Left Until Christmas! `;
+      
+        
     
-        countdownContainer.appendChild(titleEl);
+        countdownContainer.innerHTML= `Only ${daysUntilChristmas} Days Until Christmas! `;
 
         setTimeout(christmasCountdown, 1000);
   
     }
-    }
+}
     
 
 
