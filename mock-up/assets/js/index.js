691f4e6a8dfe11b7f706b65c755dcac9c3632c06
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-12-17 22:12:56 -0600

    trying to get element to append

--- mock-up/assets/js/index.js
+++ mock-up/assets/js/index.js
@@ -1,116 +1,59 @@
-// function christmasCountdown(date) {
+window.onload = function() {
+    christmasCountdown();
 
-//     //get today's date
-//     let now = new Date();
+    function christmasCountdown() {
+    //get today's date
+    let now = new Date();
 
-//     //get current month
-//     let currentMonth = (now.getMonth() + 1);
+    //get current month
+    let currentMonth = (now.getMonth() + 1);
 
-//     //get current day
-//     let currentDay = now.getDate();
+    //get current day
+    let currentDay = now.getDate();
 
-//     //calculate the year that the next Christmas will appear in
-//     let nextChristmas = now.getFullYear();
-//     if(currentMonth === 12 && currentDay > 25 ) {
-//         nextChristmas = nextChristmas + 1;
-//     }
+    //calculate the year that the next Christmas will appear in
+    let nextChristmas = now.getFullYear();
+    if(currentMonth === 12 && currentDay > 25 ) {
+        nextChristmas = nextChristmas + 1;
+    }
 
-//     let nextChristmasDate = nextChristmas + '-12-25T00:00:00.000Z';
-//     let christmasDay = new Date(nextChristmasDate);
+    let nextChristmasDate = nextChristmas + '-12-25T00:00:00.000Z';
+    let christmasDay = new Date(nextChristmasDate);
 
-//         //Get the difference in seconds between the two days.
-//         var diffSeconds = Math.floor((christmasDay.getTime() - now.getTime()) / 1000);
+        //Get the difference in seconds between the two days.
+        var diffSeconds = Math.floor((christmasDay.getTime() - now.getTime()) / 1000);
  
-//         let days = 0;
-//         let hours = 0;
-//         let minutes = 0;
+        let days = 0;
+        let hours = 0;
+        let minutes = 0;
     
      
-//         //Don't calculate the time left if it is Christmas day.
-//         if(currentMonth != 12 || (currentMonth == 12 && currentDay != 25)){
-//             //Convert these seconds into days, hours, minutes, seconds.
-//             let days = Math.floor(diffSeconds / (3600*24));
-//             diffSeconds  -= days * 3600 * 24;
-//             let hours   = Math.floor(diffSeconds / 3600);
-//             diffSeconds  -= hours * 3600;
-//             let minutes = Math.floor(diffSeconds / 60);
-//             diffSeconds  -= minutes * 60;
+        //Don't calculate the time left if it is Christmas day.
+        if(currentMonth != 12 || (currentMonth == 12 && currentDay != 25)){
+            //Convert these seconds into days, hours, minutes, seconds.
+            let days = Math.floor(diffSeconds / (3600*24));
+            diffSeconds  -= days * 3600 * 24;
+            let hours   = Math.floor(diffSeconds / 3600);
+            diffSeconds  -= hours * 3600;
+            let minutes = Math.floor(diffSeconds / 60);
+            diffSeconds  -= minutes * 60;
           
-//         }
-//         console.log(days, hours, minutes);
+        }
+        console.log(days, hours, minutes);
 
-//         let daysUntilChristmas = christmasCountdown().days + ':' + christmasCountdown().hours + ':' + christmasCountown().minutes;
-//         console.log(daysUntilChristmas)
-//         let countdownContainer = document.querySelector('#countdown');
-//         let  titleEl = document.createElement("h1");
-//         titleEl.classList.add('cta-title');
-//         titleEl.textContent = `${daysUntilChristmas} Day's Left Until Christmas! `
+        let daysUntilChristmas = days + ':' + hours + ':' + minutes;
+        console.log(daysUntilChristmas)
+        let countdownContainer = document.getElementById('#countdown');
+        let  titleEl = document.createElement("h1");
+        titleEl.classList.add('cta-title');
+        titleEl.textContent = `${daysUntilChristmas} Day's Left Until Christmas! `;
     
-//         countdownContainer.appendChild(titleEl);
+        countdownContainer.appendChild(titleEl);
 
-//         setTimeout(calculateChristmasCountdown, 1000);
-//     }
+        setTimeout(christmasCountdown, 1000);
+  
+    }
+    }
     
 
 
-
-
-// document.onload(christmasCountdown());
-
-// * Our JavaScript function, which calculates the days, hours,
-// * minutes and seconds left until Christmas day.
-// */
-function calculateChristmasCountdown(){
-   
-   //Get today's date.
-   var now = new Date();
-
-   //Get the current month. Add a +1 because
-   //getMonth starts at 0 for January.
-   var currentMonth = (now.getMonth() + 1);
-
-   //Get the current day of the month.
-   var currentDay = now.getDate();
-
-   //Work out the year that the next Christmas
-   //day will occur on.
-   var nextChristmasYear = now.getFullYear();
-   if(currentMonth == 12 && currentDay > 25){
-       //This year's Christmas Day has already passed.
-       nextChristmasYear = nextChristmasYear + 1;
-   }
-
-   var nextChristmasDate = nextChristmasYear + '-12-25T00:00:00.000Z';
-   var christmasDay = new Date(nextChristmasDate);
-
-   //Get the difference in seconds between the two days.
-   var diffSeconds = Math.floor((christmasDay.getTime() - now.getTime()) / 1000);
-
-   var days = 0;
-   var hours = 0;
-   var minutes = 0;
-   var seconds = 0;
-
-   //Don't calculate the time left if it is Christmas day.
-   if(currentMonth != 12 || (currentMonth == 12 && currentDay != 25)){
-       //Convert these seconds into days, hours, minutes, seconds.
-       days = Math.floor(diffSeconds / (3600*24));
-       diffSeconds  -= days * 3600 * 24;
-       hours   = Math.floor(diffSeconds / 3600);
-       diffSeconds  -= hours * 3600;
-       minutes = Math.floor(diffSeconds / 60);
-       diffSeconds  -= minutes * 60;
-       seconds = diffSeconds;
-   }
-
-   //Add our counts to their corresponding HTML elements.
-   document.getElementById('days').innerHTML = days + ' Days';
-   document.getElementById('hours').innerHTML = hours + ' Hours';
-   document.getElementById('minutes').innerHTML = minutes + ' Minutes';
-   document.getElementById('seconds').innerHTML = seconds + ' Seconds';
-
-   //Recursive call after 1 second using setTimeout
-   setTimeout(calculateChristmasCountdown, 1000);
-}
-
-calculateChristmasCountdown();
