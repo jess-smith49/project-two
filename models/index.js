ae0b96bcbf0071b643bbb55b4f0d3274cdcbb273
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-12-17 18:31:23 -0600

    HTML inserted into handlebars

--- models/index.js
+++ models/index.js
@@ -1,40 +1,80 @@
-//Import All Models
 const User = require('./User');
 const Drink = require('./Drink');
 const Recipe = require('./Recipe');
 const List = require('./List');
 const Group = require('./Group');
 const Category = require('./Category');
+const GroupUser = require('./GroupUser');
 
 
 //Creating Associations
 
 //Users belong to many groups
-User.belongsToMany(Group,{
-    foreignKey: 'group_id'
+User.belongsToMany(Group, {
+    through: GroupUser,
+    as: 'group_user',
+    foreignKey: 'user_id'
 });
 
 //Groups belong to many Users
-Group.belongToMany(User, {
-    foreingKey: 'user_id'
+Group.belongsToMany(User, {
+    through: GroupUser,
+    as: 'group_user',
+    foreignKey: 'group_id'
 });
 
+/*=================================================*/
+//Drink can belong belong to one user
+Drink.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-//List belong to one user through group
+//user can have many drinks------FK
+User.hasMany(Drink);
 
-//Drink belong to one user through group
+//Drink can belong to one group
+Drink.belongsTo(Group, {
+    foreignKey: 'group_id'
+});
 
-//Recipe belong to one user through group
+//Group has many drinks
+Group.hasMany(Drink);
 
+/*===============================================*/
+Recipe.belongsTo(Group, {
+    foreignKey:'group_id'
+});
 
+User.hasMany(Recipe);
 
+Recipe.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
+Group.hasMany(Recipe);
 
+/*===============================================*/
 
+List.belongsTo(Group,{
+    foreignKey: 'group_id'
+});
+User.hasMany(List);
 
+User.hasMany(Recipe);
 
+List.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
+Group.hasMany(List);
 
+/*===============================================*/
+
+List.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+
+Category.hasMany(List);
 
 
 //Exporting the Models
