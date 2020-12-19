dd3230cdfb4bfa7511995c1e8c6795710c7d784e
jess-smith49 <js.jessicasmith774@gmail.com>
2020-12-19 17:13:13 -0600

    update framework for recipe, drink and list

---
+++ public/javascript/edit-recipe.js
@@ -0,0 +1,22 @@
+async function editRecipeHandler(event) {
+    event.preventDefault();
+
+    const response = await fetch(, {
+        method: 'PUT',
+        body: JSON.stringify({
+            
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if(response.ok){
+        document.location.replace();
+    }
+    else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector().addEventListener('submit', editRecipeHandler)
