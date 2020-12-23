b494648aaeaf8430d2433f274b0bf570ca526f06
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-12-23 11:30:10 -0600

    add item form appearing

--- public/javascript/group-buttons.js
+++ public/javascript/group-buttons.js
@@ -1,23 +1,23 @@
-const addContainer = document.getElementById("add-container");
 const editContainer = document.getElementById('edit-container');
 
 
 let addEvent = document.querySelector('#newList');
 
-insertAddInput = function()  {
-let addInput = 
-`
-    <label for="newListName">Item Name</label>
-    <input type="text" placeholder="Ex: Jim Bob's List" name="newListName" id="newListName" class="form-input" />
-    <label for="newListItems">Item Name</label>
-    <input type="text" placeholder="Ex: Hot Cheetos" name="newListItems" id="newListItems" class="form-input" />
-`
-    addContainer.append(addInput)
+insertAddInput = function () {
+    const addContainer = document.getElementById("add-container");
+    let addInput = `<div class="inputContainer">
+            <label for="newListName">Item Name</label>
+            <input type="text" placeholder="Ex: Jim Bob's List" name="newListName" id="newListName" class="form-input" />
+            <label for="newListItems">Item Name</label>
+            <input type="text" placeholder="Ex: Hot Cheetos" name="newListItems" id="newListItems" class="form-input" />
+            <button class="add-list-item" id="newList">Add Item to Your List</button>
+            </div>`
+    addContainer.innerHTML = addInput;
     console.log(addInput)
 };
 
-addEvent.onclick = function() {
-        return insertAddInput();
+addEvent.onclick = function () {
+    return insertAddInput();
 }
 
 
