4500383be1fb27090fb7e4170548b1da5cc56bbb
Mona Lisa <joannagrosshans2@gmail.com>
2020-12-22 21:59:39 -0600

    mylists, mydrinks tweaked

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -9,9 +9,9 @@ router.get('/', (req, res) => {
 //get all recipes
 router.get('/recipes', (req, res) => {
     Recipe.findAll({
-        where: {
-            user_id: req.session.user_id
-        },
+        // where: {
+        //     user_id: req.session.user_id
+        // },
         attributes: ['id', 'recipe_name', 'ingredients', 'instructions'],
         include: [
             {
@@ -33,9 +33,9 @@ router.get('/recipes', (req, res) => {
 //get all drinks
 router.get('/drinks', (req, res) => {
     Drink.findAll({
-        where: {
-            user_id: req.session.user_id
-        },
+        // where: {
+        //     user_id: req.session.user_id
+        // },
         attributes: ['id', 'drink_name', 'ingredients', 'instructions'],
         include: [
             {
@@ -56,9 +56,9 @@ router.get('/drinks', (req, res) => {
 //get all lists
 router.get('/lists', (req, res) => {
     List.findAll({
-        where: {
-            user_id: req.session.user_id
-        },
+        // where: {
+        //     user_id: req.session.user_id
+        // },
         attributes: ['id', 'list_name', 'list_items'],
         include: [
             {
