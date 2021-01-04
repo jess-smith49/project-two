89e6514494b73f59491f46a0688a73d94b0432ad
jess-smith49 <js.jessicasmith774@gmail.com>
2021-01-03 20:35:58 -0600

    changed hardcoding

--- config/connection.js
+++ config/connection.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize('christmas_db', 'root', 'Bluebayou1!', {
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
