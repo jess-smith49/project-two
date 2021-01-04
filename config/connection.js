9a994ecb98ed119fb81fa047ad5bec5fdd4cf24b
jess-smith49 <js.jessicasmith774@gmail.com>
2021-01-03 20:29:07 -0600

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,16 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// const sequelize = process.env.JAWSDB_URL
-//     ? new Sequelize(process.env.JAWSDB_URL)
-//     : new Sequelize('christmas_db', 'root', 'Bluebayou1!', {
-//         host: 'localhost',
-//         dialect: 'mysql',
-//         port: 3306
-//     });
+ const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize('christmas_db', 'root', 'Bluebayou1!', {
+        host: 'localhost',
+        dialect: 'mysql',
+         port: 3306
+     });
 
 
-const sequelize = process.env.JAWSDB_URL
+/*const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
@@ -20,4 +20,4 @@ const sequelize = process.env.JAWSDB_URL
         port: 3306
     });
     
-module.exports = sequelize;
+module.exports = sequelize;*/
